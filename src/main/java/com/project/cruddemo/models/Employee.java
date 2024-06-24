6bbe2dfad5f0896527d4810c6ddb48820b94906f
Cristian <cristianvillar252@gmail.com>
2024-06-24 12:19:12 -0300

    Adds main entity
    The client needs for a CRUD system for his employees.

---
+++ src/main/java/com/project/cruddemo/models/Employee.java
@@ -0,0 +1,35 @@
+package com.project.cruddemo.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name="employee")
+@Data
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name ="email")
+    private String email;
+
+    public Employee(int id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public Employee() {
+
+    }
+}
