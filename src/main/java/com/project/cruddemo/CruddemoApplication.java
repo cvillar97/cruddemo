d85530c010c7d7f4456f5adaa2eab713cfa7e56b
Cristian <cristianvillar252@gmail.com>
2024-08-05 19:50:30 -0300

    Adds custom Swagger-ui textfields

--- src/main/java/com/project/cruddemo/CruddemoApplication.java
+++ src/main/java/com/project/cruddemo/CruddemoApplication.java
@@ -1,7 +1,11 @@
 package com.project.cruddemo;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CruddemoApplication {
@@ -10,4 +14,14 @@ public class CruddemoApplication {
 		SpringApplication.run(CruddemoApplication.class, args);
 	}
 
+	@Bean
+	public OpenAPI customOpenAPI() {
+		return new OpenAPI()
+				.info(new Info()
+						.title("Employee Repository API")
+						.version("1.0")
+						.description("Employee CRUD API")
+						.termsOfService("http://swagger.io/terms/")
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
 }
