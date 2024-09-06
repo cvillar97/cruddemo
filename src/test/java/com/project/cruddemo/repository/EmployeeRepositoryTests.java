29087bd410c1d20ccc35cfc9b35525ab735ba308
Cristian <cristianvillar252@gmail.com>
2024-09-06 17:57:20 -0300

    Adds tests for remaining methods
    Tests for: findAll, findById, findByLastName, Update and DeleteById.

--- src/test/java/com/project/cruddemo/repository/EmployeeRepositoryTests.java
+++ src/test/java/com/project/cruddemo/repository/EmployeeRepositoryTests.java
@@ -37,3 +37,93 @@ public class EmployeeRepositoryTests {
     assertThat(savedEmployee.getFirstName()).isEqualTo("Hugo");
     assertThat(savedEmployee.getLastName()).isEqualTo("Valles");
     }
+
+    @Test
+    public void EmployeeRepository_FindAll_ReturnsMoreThanOneEmployee() {
+        Employee employee1 = Employee.builder()
+                .firstName("Juan")
+                .lastName("Villegas").build();
+        Employee employee2 = Employee.builder()
+                .firstName("Roca")
+                .lastName("Johnson").build();
+
+        Employee saved1 = underTest.save(employee1);
+        Employee saved2 = underTest.save(employee2);
+
+        List<Employee> employees = underTest.findAll();
+
+        assertThat(employees).isNotNull();
+        assertThat(employees.size()).isEqualTo(2);
+    }
+
+    @Test
+    public void EmployeeRepository_FindById_ReturnsTheSavedEmployeeById() {
+
+        Employee theEmployee = Employee.builder()
+                .firstName("Hugo")
+                .lastName("Valles")
+                .build();
+
+        underTest.save(theEmployee);
+
+        int theEmployeeId = theEmployee.getId();
+
+        Employee foundEmployee = underTest.findById(theEmployeeId).get();
+
+        assertThat(foundEmployee).isNotNull();
+        assertThat(foundEmployee.getId()).isEqualTo(theEmployeeId);
+    }
+
+    @Test
+    public void EmployeeRepository_FindByLastName_ReturnsEmployeeNotNull() {
+
+        Employee theEmployee = Employee.builder()
+                .firstName("Hugo")
+                .lastName("Valles")
+                .build();
+
+        underTest.save(theEmployee);
+
+        String theEmployeeLastName = theEmployee.getLastName();
+
+        Employee foundEmployee = underTest.findByLastName(theEmployeeLastName).get();
+
+        assertThat(foundEmployee).isNotNull();
+        assertThat(foundEmployee.getFirstName()).isEqualTo("Hugo");
+    }
+
+    @Test
+    public void EmployeeRepository_Update_ReturnsUpdatedEmployee() {
+
+        Employee theEmployee = Employee.builder()
+                .firstName("Hugo")
+                .lastName("Valles")
+                .build();
+
+        underTest.save(theEmployee);
+        theEmployee.setFirstName("Carlos");
+        theEmployee.setLastName("Juarez");
+
+        Employee updatedEmployee = underTest.save(theEmployee);
+
+        assertThat(updatedEmployee).isNotNull();
+        assertThat(updatedEmployee.getFirstName()).isEqualTo("Carlos");
+        assertThat(updatedEmployee.getLastName()).isEqualTo("Juarez");
+    }
+
+    @Test
+    public void EmployeeRepository_DeleteById_ReturnsEmployeeIsEmpty() {
+
+        Employee theEmployee = Employee.builder()
+                .firstName("Hugo")
+                .lastName("Valles")
+                .build();
+
+        underTest.save(theEmployee);
+
+        underTest.deleteById(theEmployee.getId());
+        Optional<Employee> employeeReturn = underTest.findById(theEmployee.getId());
+
+        assertThat(employeeReturn).isEmpty();
+    }
+}
