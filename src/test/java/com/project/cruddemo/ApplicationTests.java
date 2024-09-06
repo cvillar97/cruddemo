7765e96743856437434527287c1cee9c68620fbd
Cristian <cristianvillar252@gmail.com>
2024-09-06 18:40:21 -0300

    ApplicationTests class

---
+++ src/test/java/com/project/cruddemo/ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.project.cruddemo;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApplicationTests {
+
+    @Test
+    public void contextLoads(){
+    }
+
+}
