516cc3fb8252d913096efbd5049567e47d966d9f
Cristian <cristianvillar252@gmail.com>
2024-09-06 18:02:56 -0300

    Creates EmployeeServiceTests
    As I want to unit test the Service class, I'm going to mimic every interaction with the Repository layer using a mock of EmployeeRepository wich is "injected" into the EmployeeServiceImpl object.

---
+++ src/test/java/com/project/cruddemo/service/EmployeeServiceTests.java
@@ -0,0 +1,29 @@
+package com.project.cruddemo.service;
+
+import com.project.cruddemo.exceptions.EmployeeNotFoundException;
+import com.project.cruddemo.models.Employee;
+import com.project.cruddemo.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeServiceTests {
+
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    private EmployeeServiceImpl underTest;
+
